5cfc9e7b2091aa9720e9abe1def929c2e007464a
ho ngoc man <hongocman0501@gmail.com>
2017-07-09 14:30:34 +0700

    Them dinh nghia operator -=

--- Project1/Number.h
+++ Project1/Number.h
@@ -17,6 +17,7 @@ public:
 	Number<T> operator *(Number<T> ); 
 	Number<T> operator /(Number<T> ); 
 	Number<T> operator +=(Number<T> );
+	Number<T> operator -=(Number<T> );
 	friend ostream& operator<<<>(ostream&, const Number<T>&);
 	friend istream& operator>><>(istream&, const Number<T>&);
 	bool operator ==(T );
